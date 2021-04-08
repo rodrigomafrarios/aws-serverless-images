be5f69aa3d4869fa95a985f63204f210fc222a6b
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2021-04-08 10:29:07 -0300

    feat: ensure ImageValidatorAdapter return true if validator returns true

--- api/utils/image-validator-adapter.ts
+++ api/utils/image-validator-adapter.ts
@@ -1,7 +1,10 @@
 import { ImageValidator } from '@/presentation/interfaces/image-validator'
 
 export class ImageValidatorAdapter implements ImageValidator {
-  isValid (image: any): boolean {
+  isValid (image: Buffer): boolean {
+    if (Buffer.isBuffer(image)) {
+      return Buffer.byteLength(image) > 0 && Buffer.byteLength(image) <= 1048576
+    }
     return false
   }
 }
--- tests/utils/image-validator-adapter.spec.ts
+++ tests/utils/image-validator-adapter.spec.ts
@@ -1,13 +1,23 @@
 import { ImageValidatorAdapter } from '@/utils/image-validator-adapter'
+import path from 'path'
+import fs from 'fs'
 
+const image = fs.readFileSync(path.resolve(path.join(__dirname,'../presentation/mocks/image.png')))
+const imageOutOfLimitSize = fs.readFileSync(path.resolve(path.join(__dirname,'../presentation/mocks/AsterNovi-belgii-flower-1mb.jpg')))
 const factory = (): ImageValidatorAdapter => {
   return new ImageValidatorAdapter()
 }
 
 describe('ImageValidator - Adapter', () => {
-  test('Should return false if validator returns false', async () => {
+  test('Should return false if validator returns false', () => {
     const sut = factory()
-    const isValid = sut.isValid('')
+    const isValid = sut.isValid(imageOutOfLimitSize)
     expect(isValid).toBe(false)
+  })
+
+  test('Should return true if validator returns true', () => {
+    const sut = factory()
+    const isValid = sut.isValid(image)
+    expect(isValid).toBe(true)
   })
 })
