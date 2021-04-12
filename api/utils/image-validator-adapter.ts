0b9f4fcb21351caa5381a3a18e3ff0b0bde1578d
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2021-04-12 13:29:47 -0300

    refactor: remove useless condition on ImageValidatorAdapter isValid method

--- api/utils/image-validator-adapter.ts
+++ api/utils/image-validator-adapter.ts
@@ -2,9 +2,7 @@ import { ImageValidator } from '@/presentation/interfaces/image-validator'
 
 export class ImageValidatorAdapter implements ImageValidator {
   isValid (image: Buffer): boolean {
-    if (Buffer.isBuffer(image)) {
-      return Buffer.byteLength(image) > 0 && Buffer.byteLength(image) <= 1048576
-    }
-    return false
+    const isBufferValid = Buffer.byteLength(image) > 0 && Buffer.byteLength(image) <= 1048576
+    return isBufferValid
   }
 }
