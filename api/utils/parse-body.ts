5e7a0b9cbff2bd3753092538b49b645e8fb85dc7
Rodrigo Mafra <rodrigomafrarios@gmail.com>
2021-04-18 19:28:04 -0300

    feat: parse body image to base64

--- api/utils/parse-body.ts
+++ api/utils/parse-body.ts
@@ -1,5 +1,5 @@
 export class ParseBody {
-  async toBase64 (image: string): Promise<Buffer> {
+  async toBase64 (image: any): Promise<Buffer> {
     const base64Image = Buffer.from(image.replace(/^image\/\w+;base64,/, ''), 'base64')
     return base64Image
   }
