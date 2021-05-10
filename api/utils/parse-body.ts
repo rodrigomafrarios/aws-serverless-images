efee065e66a82aa678869b2581f42d19abf6d556
Rodrigo <rodrigomafrarios@gmail.com>
2021-05-10 11:34:13 -0300

    refactor: remove unsued files

--- api/utils/parse-body.ts
+++
@@ -1,6 +0,0 @@
-export class ParseBody {
-  async toBase64 (image: any): Promise<Buffer> {
-    const base64Image = Buffer.from(image.replace(/^image\/\w+;base64,/, ''), 'base64')
-    return base64Image
-  }
-}
