298cc8100f6d7c4625df9663671b7a5787d0886a
Rodrigo <rodrigomafrarios@gmail.com>
2021-05-10 15:21:00 -0300

    refactor: move upload s3 image params to imageAdapter

--- api/main/adapters/image-adapter.ts
+++ api/main/adapters/image-adapter.ts
@@ -1,16 +1,24 @@
-export interface ImageBuffer {
-  buffer: Buffer
-  type: string
-  fileName: string
+export interface UploadS3ImageParams {
+  image: {
+    Bucket: string
+    Key: string
+    ContentType: string
+    ContentEncoding: string
+    Body: Buffer
+  }
 }
 
-export const imageAdapt = (image: any): ImageBuffer => {
+export const imageAdapt = (image: string): UploadS3ImageParams => {
   const buffer = Buffer.from(image.replace(/^data:image\/\w+;base64,/, ''), 'base64')
   const type = image.split(';')[0].split('/')[1]
   const fileName = `image-${new Date().getTime()}`
   return {
-    buffer,
-    type,
-    fileName
+    image: {
+      Bucket: process.env.IMAGE_BUCKET,
+      Key: `${fileName}.${type}`,
+      ContentType: `image/${type}`,
+      ContentEncoding: 'base64',
+      Body: buffer
+    }
   }
 }
--- api/main/lambdas/upload-image/handler.ts
+++ api/main/lambdas/upload-image/handler.ts
@@ -1,25 +1,15 @@
-import { makeUploadImageController } from '@/main/factories/controllers/image/upload-image-controller-factory'
 import 'source-map-support/register'
-
 import type { ValidatedEventAPIGatewayProxyEvent } from '@/libs/apiGateway'
 import { middyfy } from '@/libs/lambda'
 import schema from '@/main/lambdas/upload-image/schema'
 import { imageAdapt } from '@/main/adapters/image-adapter'
 import { lambdaAdapt } from '@/main/adapters/lambda-adapter'
+import { makeUploadImageController } from '@/main/factories/controllers/image/upload-image-controller-factory'
 
 const uploadImage: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   const controller = makeUploadImageController()
-  const { buffer, type, fileName } = imageAdapt(event.body.file)
-  const httpResponse = lambdaAdapt(controller)({
-    image: {
-      Bucket: process.env.IMAGE_BUCKET,
-      Key: `${fileName}.${type}`,
-      ContentType: `image/${type}`,
-      ContentEncoding: 'base64',
-      Body: buffer
-    }
-  })
-
+  const httpRequest = imageAdapt(event.body.file)
+  const httpResponse = lambdaAdapt(controller)(httpRequest)
   return httpResponse
 }
 
