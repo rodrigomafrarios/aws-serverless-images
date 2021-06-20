8b7854e0b2495e93db569a6f625f68d1b956484e
Rodrigo <rodrigomafrarios@gmail.com>
2021-06-20 12:56:58 -0300

    feat: add iam role to create thumbnail function

--- api/main/lambdas/create-thumbnail/index.ts
+++ api/main/lambdas/create-thumbnail/index.ts
@@ -2,6 +2,10 @@ import { handlerPath } from '@/libs/handlerResolver'
 
 export default {
   handler: `${handlerPath(__dirname)}/main.handler`,
+  role: "${ssm:${self:custom.stage}-create-thumbnail-iam-role}",
+  environment: {
+    THUMBNAIL_BUCKET: "${ssm:${self:custom.stage}-create-thumbnail-bucket}"
+  },
   events: [{
     s3: {
       bucket: "${ssm:${self:custom.stage}-upload-image-bucket}",
