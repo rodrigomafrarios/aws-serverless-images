a3cc47134cb75a0e01eee269babd948b09cd61c2
Rodrigo Mafra <rodrigo.mafra@zenklub.com>
2021-08-10 20:39:34 -0300

    fix: import path

--- api/main/factories/controllers/thumbnail/create-thumbnail-controller-factory.ts
+++ api/main/factories/controllers/thumbnail/create-thumbnail-controller-factory.ts
@@ -1,6 +1,6 @@
 import { Controller } from '@/presentation/interfaces/controller'
-import { makeLogControllerDecorator } from '../../../factories/controllers/log/log-controller-decorator-factory'
 import { CreateThumbnailController } from '@/presentation/controllers/thumbnail/create-thumbnail-controller'
+import { makeLogControllerDecorator } from '../log/log-controller-decorator-factory'
 import { makeStorageCreateThumbnail } from '../../usecases/storage-create-thumbnail-factory'
 
 export const makeCreateThumbnailController = (): Controller => {
