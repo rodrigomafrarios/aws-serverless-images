af959f8cf8cb8a9387a7b153abb4c9a11df81920
Rodrigo Mafra <rodrigo.mafra@zenklub.com>
2021-08-10 10:25:06 -0300

    chore: add jest coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
   roots: ['<rootDir>/tests'],
   collectCoverageFrom: [
     '<rootDir>/api/**/*.ts',
